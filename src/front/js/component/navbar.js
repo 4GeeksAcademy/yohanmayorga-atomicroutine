cc4e81f6125ae3f5526efada0051089d462881f6
Yohan <102123553+yohanmayorga@users.noreply.github.com>
2023-10-27 20:01:12 -0400

    Update navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-sm navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<a href="/" className="navbar-brand mb-0">
+					Atomic Routine
+				</a>
+				<button
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					className="navbar-toggler"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a href="/comofunciona" className="nav-link">
+								Cómo funciona
+							</a>
+						</li>
+						<li className="nav-item active">
+							<a href="#" className="nav-link">
+								Herramientas
+							</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a href="#"
+								className="nav-link dropdown-toggle"
+								id="navbarDropdown"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false">
+								Recursos
+							</a>
+							<ul className="dropdown-menu" arial-aria-labelledby="navbarDropdown">
+								<li>
+									<a href="#" className="dropdown-item">Recurso 1</a>
+								</li>
+								<li>
+									<a href="#" className="dropdown-item">Recurso 2</a>
+								</li>
+								<li>
+									<a href="#" className="dropdown-item">Recurso 3</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
 				</div>
+				<a href="/login">
+					<button type="button" className="button" href="/login">
+						Ingresa
+					</button>
+				</a>
 			</div>
 		</nav>
 	);
